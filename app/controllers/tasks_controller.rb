9ffa9d508df95398207ead2bb4395567721aa426
yuri <yuri.niiskuneiti@gmail.com>
2017-04-19 08:22:54 +0000

    add authentication

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,29 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
-    @tasks = Task.all
+    @user = current_user
+    @tasks = @user.tasks.order('created_at DESC')
   end
   
   def show
     @task = Task.find(params[:id])
+    @task.user_id = current_user.id
+     
+    
   end
   
   def new
     @task = Task.new
+ #   @user = User.find(params[:id])
   end
   
   def create
     @task = Task.new(task_params)
+    @user = current_user
+    @task.user_id = @user.id   
+#  @user = User.find(params[:id])
     
     if @task.save
       flash[:success] = 'Taskが正常に追加されました'
@@ -44,12 +55,19 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_path
   end
   
   private
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+   def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
   end
   
 end
