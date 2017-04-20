67e5b1cfc72d82253cae81c88f35c85eb681fb78
yuri <yuri.niiskuneiti@gmail.com>
2017-04-20 02:17:58 +0000

    revision2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
-  before_action :correct_user, only: [:show, :create]
+  before_action :correct_user, only: [:show]
   
   
   def show
